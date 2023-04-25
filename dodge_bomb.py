4c8499676f5b02a838412755157a7ffbc0fa8bf7
Shusuke.C <c0a22098ea@edu.teu.ac.jp>
2023-04-25 17:48:28 +0900

    コメント #2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,7 +16,7 @@ delta = {
         }
 
 
-def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:  # 練習5
     """
     オブジェクトが画面内or画面外を判定し　真理値タプルを返す関数
     引数1:　画面surfaceのRect
@@ -39,15 +39,15 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = (900, 400)
-    #kk_imgs = [pg.transform.rotozoom(kk_img , 45, 1.0), pg.transform.rotozoom(kk_img , 90, 1.0), pg.transform.rotozoom(kk_img , 135, 1.0), pg.transform.rotozoom(kk_img , 180, 1.0), pg.transform.rotozoom(kk_img , -135, 1.0), pg.transform.rotozoom(kk_img , -90, 1.0), pg.transform.rotozoom(kk_img , -45, 1.0)]
     bb_img = pg.Surface((20, 20)) 
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習1
-    bb_img.set_colorkey((0, 0, 0))  #練習1
+    bb_img.set_colorkey((0, 0, 0))  # 練習1
     x, y = random.randint(0,1600), random.randint(0,900)  #練習２
-    #screen.blit(bb_img, [x, y])  #練習2
+    #screen.blit(bb_img, [x, y])  # 練習2
     vx, vy = +1, +1
     bb_rct = bb_img.get_rect()
     bb_rct.center = (x, y)
+    # 追加課題2
     bb_imgs = []
     accs = [a for a in range(1,11)]  #  加速度のリスト
     for r in range(1,11):  # 拡大爆弾のリスト
@@ -68,7 +68,7 @@ def main():
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
-        if check_bound(screen.get_rect(), kk_rct) != (True, True):  # こうかとんの画面判定
+        if check_bound(screen.get_rect(), kk_rct) != (True, True):  # 練習5 こうかとんの画面判定
             for k, mv in delta.items():
                 if key_lst[k]:
                     kk_rct.move_ip(-mv[0], -mv[1])
@@ -81,10 +81,11 @@ def main():
         if not tate:  # 縦方向にはみ出ていたら
             vy *= -1
         screen.blit(bb_img, bb_rct)  
-        bb_img= bb_imgs[min(tmr//1000, 9)]  #時間によって爆弾を大きくする
-        avx, avy = vx*accs[min(tmr//1000, 9)], vy*accs[min(tmr//1000, 9)]
-        bb_rct.move_ip(vx + avx, vy + avy)   
-        if kk_rct.colliderect(bb_rct):
+        # 追加課題２
+        bb_img= bb_imgs[min(tmr//1000, 9)]  # 時間によって爆弾を大きくする
+        avx, avy = vx*accs[min(tmr//1000, 9)], vy*accs[min(tmr//1000, 9)]  # 時間によって加速度を大きくする
+        bb_rct.move_ip(vx + avx, vy + avy)  # 初速度に加速度をたす   
+        if kk_rct.colliderect(bb_rct):  # 練習6
             kk_img = pg.image.load("ex02/fig/8.png")
             return
 
