5a09532f78a2253983191a92feab89f47814853a
Shusuke.C <c0a22098ea@edu.teu.ac.jp>
2023-04-25 17:42:57 +0900

    コメント #1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -20,7 +20,7 @@ def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
     オブジェクトが画面内or画面外を判定し　真理値タプルを返す関数
     引数1:　画面surfaceのRect
-    引数2:　効果トン　または　爆弾surfaceのRect
+    引数2:　こうかとん　または　爆弾surfaceのRect
     戻り値　横方向　縦方向のはみ出し判定結果　（画面会　True/g画面外　False)
     """
     yoko, tate = True, True
